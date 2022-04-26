03fca36766d5c06774aec03cc45b4eee1eb4b434
Tati1511 <102051224+Tati1511@users.noreply.github.com>
2022-04-26 13:48:33 -0400

    while_counter

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut counter: i32 = 0; 
+    
+    while counter < 4 {
+        counter += 1;
+        println!("Inside the while loop, already looped {} times",counter);
+    }
+
+}
